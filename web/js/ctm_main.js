15d9a534bffbce6f5c84cfe0ccc037f447b036ad
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2025-08-07 00:46:33 +0000

    wip

---
+++ web/js/ctm_main.js
@@ -0,0 +1,219 @@
+/***
+   ctm_main.c
+
+***/
+
+var viewermap;
+
+jQuery(document).ready(function() {
+
+  frameHeight=window.innerHeight;
+  frameWidth=window.innerWidth;
+
+  if( screen.width <= 480 ) {
+    window.console.log("OH NO.. I am on Mini.."+screen.width);
+    //location.href = '/mobile.html';
+  }
+
+  viewermap=setup_viewer();
+
+  $(".popup").hide();
+
+  $(".openpop").click(function(e) {
+    e.preventDefault();
+    $("iframe").attr("src",$(this).attr('href'));
+    $(".links").fadeOut('slow');
+    $(".popup").fadeIn('slow');
+  });
+
+/*
+  $(".close").click(function() {
+    $(this).parent().fadeOut("slow");
+    $(".links").fadeIn("slow");
+  });
+*/
+
+  $("#searchType").change(function () {
+      var s= document.getElementById("searchType").value;
+      if( s == 'lineClick' || s == 'areaClick' ) { 
+        $( "#zMode-elevClick" ).attr("disabled","disabled");
+        } else {
+          $( "#zMode-elevClick" ).attr("disabled",false);
+      }
+      var funcToRun = $(this).val();
+      if (funcToRun != "") {
+        window[funcToRun]();
+      }
+  });
+  $("#searchType").trigger("change");
+
+  $("#selectModelType").change(function () {
+      var model = $(this).val();
+      remove_all_models();
+      load_selected_model(model);
+      refreshModelDescription(model);
+      set_point_latlons_special();
+      // special case.. elygtl:ely or elygtl:taper
+      var v=document.getElementById('zrange').style.display;
+      var vv=document.getElementById('floors').style.display;
+      var ely=model.includes("elygtl:ely");
+      var taper=model.includes("elygtl:taper");
+
+      if(ely) {
+         if( v == "none" ) { 
+           document.getElementById('zrange').style.display="block";
+         }
+         if(vv != "none") {
+           document.getElementById('floors').style.display="none";
+         }
+         set_zrange_presets();
+	 set_zrange_stop(350);     
+         return;
+      }
+      if(taper) {
+         if(v == "none") {
+           document.getElementById('zrange').style.display="block";
+         }
+         if(vv == "none") {
+           document.getElementById('floors').style.display="block";
+         }
+         set_zrange_presets();
+         set_zrange_stop(700);
+         set_floors_presets();
+         return;
+      }
+      if(v=="block" && !ely && !taper) {
+         document.getElementById('zrange').style.display="none";
+         set_zrange_presets();
+      }
+      if(vv=="block" && !taper) {
+         document.getElementById('floors').style.display="none";
+      }
+  });
+
+  $("#zModeType").change(function () {
+     set_zrange_presets();
+     reset_presets();
+  });
+
+  $("#areaDataTypeTxt").change(function () {
+      reset_presets();
+  });
+
+  $('#processMPTableList li').click(function() {
+      var v=$(this).data('id');
+      processMPTable(v);
+  });
+
+  $('#processMetaPlotResultTableList li').click(function() {
+      var v=$(this).data('id');
+      processMetaPlotResultTable(v);
+  });
+
+  // setup the iframe for profile page
+  $("#plotProfileBtn").click(function(e) {
+      plotly_profile_run();
+  });
+
+  $("#ctm-model-cfm").on('click', function () {
+      if ($(this).prop('checked')) {
+          CXM.showCFMFaults(viewermap);
+      } else {
+          CXM.hideCFMFaults(viewermap);
+      }
+  });
+    
+  $("#ctm-model-gfm").change(function() {
+    if ($("#ctm-model-gfm").prop('checked')) { 
+        CXM.showGFMRegions(viewermap);
+        } else {
+            CXM.hideGFMRegions(viewermap);
+    } 
+  });     
+
+  $("#ctm-model-ctm").change(function() {
+    if ($("#ctm-model-ctm").prop('checked')) {
+        CXM.showCTMRegions(viewermap);
+        } else {
+            CXM.hideCTMRegions(viewermap);
+    }
+  });
+
+  $("#ctm-model-ctm").change(function() {
+    toggleShowModels();
+  });
+
+/***** reference popup modal *****/
+  $('#modalreference').on('show.bs.modal', function (event) {
+        var btn = $(event.relatedTarget); 
+        var ref_info=btn.data('ref');        
+	$("#modalreferencebody").html(ref_info);
+  });
+/***** info popup modal *****/
+  $('#modalinfo').on('show.bs.modal', function (event) {
+        var btn = $(event.relatedTarget); 
+        var info_info=btn.data('info');        
+	$("#modalinfobody").html(info_info);
+  });
+/***** tinyinfo popup modal *****/
+  $('#modaltinyinfo').on('show.bs.modal', function (event) {
+        var btn = $(event.relatedTarget); 
+        var tinyinfo_info=btn.data('info');        
+	$("#modaltinyinfobody").html(tinyinfo_info);
+  });
+
+/***** plotoption popup modal *****/
+  $('#modalplotoption').on('show.bs.modal', function (event) {
+    // button that triggered the modal
+        var btn = $(event.relatedTarget); 
+        var blob_btn=btn.data('blob');        
+        var blob=document.getElementById(blob_btn).innerHTML;
+        let type=updatePlotOptions(blob);
+
+ 	let json=JSON.parse(blob);
+        let myfile=json['file'];
+        // switch from ../reslut/pdffile to /ctm-explorer/result/pdffile 
+//window.console.log("  myfile is  -->", myfile);
+        MODAL_REPLOT_SRC=myfile.replace("..","/research/ctm-explorer");
+        MODAL_REPLOT_TYPE=type;
+        MODAL_REPLOT_PAR=false; // start as false initially
+//window.console.log("  new myfile is  -->", MODAL_REPLOT_SRC);
+
+    // Set the modal content dynamically
+        var modal = $(this);
+
+	$('#plotOptionIfram').attr('src',MODAL_REPLOT_SRC);
+	let h=setIframHeight("plotOptionIfram");
+
+        document.getElementById("plotoption-header").style.display="";
+
+  });
+// special case.. if onpar get changed, need to track and call refresh plot range
+  $("#plotParamTxt").change(function() {
+        MODAL_REPLOT_PAR=true;
+	// clear the min/max 
+        document.getElementById("minPlotScaleTxt").value=0;
+        document.getElementById("maxPlotScaleTxt").value=0;
+ 
+  });
+
+  $('#giveUp').click(function() {
+      document.getElementById('spinIconForArea').style.display = "none";
+      document.getElementById('spinIconForLine').style.display = "none";
+      CTM.hardReset();
+  });
+
+
+// MAIN SETUP
+  CTM.setup_model();
+  CTM.setupCTMInterface();
+  cleanResultDirectory();
+  // 11:00pm
+  setDailyCleanResult(23, 0);
+
+  $.event.trigger({ type: "page-ready", "message": "completed", });
+
+}); // end of MAIN
+
+
+
---
+++ web/js/ctm_region_util.js
@@ -0,0 +1,666 @@
+/****
+
+  ctm_region_util.js
+
+****/
+
+// information on model 
+var CTM_installed=[];
+
+// str is a blob { 'models': ['ctmh','ctms5'] }
+function makeInstallModelList(str) {
+  var blob;
+  if( str == undefined || str == "" ) {
+     window.console.log("ERROR: no return result");
+     return "";
+  }
+  if( typeof str === 'string') {
+     blob=JSON.parse(str);
+     } else {
+       blob=str;
+  }
+  var mlist=blob['models'];
+  var cnt=mlist.length;
+  var i;
+  for(i=0;i<cnt;i++) {
+    var item=mlist[i];
+    CTM_installed.push(item);
+  }
+
+  setup_modeltype();
+}
+
+function isModelInstalled(pname) {
+  var cnt=CTM_installed.length;
+  var i=0;
+  for(i=0; i<cnt;i++) {
+     if(CTM_installed[i]==pname) {
+        return 1;
+     }
+  }
+  return 0;
+}
+
+function makeModelSelection() {
+   var tb=CTM_tb['models'];
+   var cnt=tb.length;
+   var i;
+   var option;
+   var all_model_list=[];
+   for(i=0; i<cnt; i++) {
+     var item=tb[i];
+     var color=item['color'];
+     var aname=item['abb name'];
+     var mname=item['name'];
+     var pname=item['path name'];
+     // check the model directory to make sure it exists before adding 
+     // the option
+     if(isModelInstalled(pname)) {
+        var sel=document.getElementById('selectModelType');
+        option = document.createElement("option");
+        option.text = mname;
+        option.label = mname;
+        option.value= aname;
+        sel.add(option);
+        all_model_list.push(aname);
+     }
+   } 
+   // special case
+   var sel=document.getElementById('selectModelType');
+   option = document.createElement("option");
+   option.text = "-- Tiled Models --";
+   option.setAttribute("disabled", true);
+   option.value= "disabled";
+   sel.add(option);
+
+   if(isModelInstalled("ctmh") && isModelInstalled("cs248")) {
+   option = document.createElement("option");
+   option.text = "CS 248,CTM-H v15.1.1";
+   option.label = "CS 248,CTM-H v15.1.1";
+   option.value= "cs248,ctmh";
+   sel.add(option);
+   }
+
+   if(isModelInstalled("ctmhlabn") && isModelInstalled("ctmsi")) {
+   option = document.createElement("option");
+   option.text = "CTM-H LA Basin,CTM-S4.26M01";
+   option.label = "CTM-H LA Basin,CTM-S4.26M01";
+   option.value= "ctmhlabn,ctmsi";
+   sel.add(option);
+   }
+
+   if(isModelInstalled("ctms5") && isModelInstalled("cca")) {
+   option = document.createElement("option");
+   option.text = "CCA,CTM-S4.26,elygtl:ely";
+   option.label = "CCA,CTM-S4.26,elygtl:ely";
+   option.value= "cca,ctms5,elygtl:ely";
+   sel.add(option);
+   }
+
+   if(isModelInstalled("ctmsi")) {
+   option = document.createElement("option");
+   option.text = "CTM-S4.26M01,elygtl:taper";
+   option.label = "CTM-S4.26M01,elygtl:taper";
+   option.value= "ctmsi,elygtl:taper";
+   sel.add(option);
+   }
+
+   if(isModelInstalled("ctmsi")) {
+   option = document.createElement("option");
+   option.text = "CTM-S4.26M01,elygtl:ely";
+   option.label = "CTM-S4.26M01,elygtl:ely";
+   option.value= "ctmsi,elygtl:ely";
+   sel.add(option);
+   }
+
+/***
+   if(isModelInstalled("1d")) {
+     option = document.createElement("option");
+     option.text = "1D";
+     option.label = "1D";
+     option.value= "1d"; 
+     sel.add(option);
+   }
+
+   if(isModelInstalled("1d")) {
+     option = document.createElement("option");
+     option.text = "BBP1D";
+     option.text = "BBP1D";
+     option.label = "BBP1D";
+     option.value= "bbp1d"; 
+     sel.add(option);
+   }
+
+   if(isModelInstalled("ctmhlabn") && isModelInstalled("ctmhsgbn"),
+      isModelInstalled("ctmhvbn") && isModelInstalled("ctmhrbn"),
+      isModelInstalled("ctmhibbn") && isModelInstalled("ctmhsmbn"),
+      isModelInstalled("ctmhsbbn") && isModelInstalled("ctmhsbcbn"),
+      isModelInstalled("ctmhstbn") && isModelInstalled("ctmsi")) {
+        option = document.createElement("option");
+        option.text = "CTM-H All Basins, CTM-S4.26.M01";
+        option.value= "ctmhlabn,ctmhsgbn,ctmhvbn,ctmhrbn,ctmhibbn,ctmhsmbn,ctmhsbbn,ctmhsbcbn,ctmhstbn,ctmsi";
+        sel.add(option);
+   }
+
+   if(isModelInstalled("sfctm") && isModelInstalled("cca")) {
+     option = document.createElement("option");
+     option.text = "SFCTM,CCA";
+     option.label = "SFCTM,CCA";
+     option.value= "sfctm,cca"; 
+     sel.add(option);
+   }
+
+   if(isModelInstalled("sfctm") && isModelInstalled("1d")) {
+     option = document.createElement("option");
+     option.text = "SFCTM,1D";
+     option.label = "SFCTM,1D";
+     option.value= "sfctm,1d"; 
+     sel.add(option);
+   }
+
+   if(isModelInstalled("sfctm") && isModelInstalled("cca") && isModelInstalled("1d")) {
+     option = document.createElement("option");
+     option.text = "SFCTM,CCA,1D";
+     option.label = "SFCTM,CCA,1D";
+     option.value= "sfctm,cca,1d";
+     sel.add(option);
+   }
+
+// nc1d is with 1d
+   if(isModelInstalled("sfctm") && isModelInstalled("cca") && isModelInstalled("1d")) {
+     option = document.createElement("option");
+     option.text = "SFCTM,CCA,SF1D";
+     option.label = "SFCTM,CCA,SF1D";
+     option.value= "sfctm,cca,sf1d";
+     sel.add(option);
+   }
+
+
+// bbp1d is with 1d
+   if(isModelInstalled("sfctm") && isModelInstalled("cca") && isModelInstalled("1d")) {
+     option = document.createElement("option");
+     option.text = "SFCTM,CCA,BBP1D";
+     option.label = "SFCTM,CCA,BBP1D";
+     option.value= "sfctm,cca,bbp1d";
+     sel.add(option);
+   }
+
+
+   if(isModelInstalled("sjfz") && isModelInstalled("1d")) {
+     option = document.createElement("option");
+     option.text = "SJFZ,1D";
+     option.label = "SJFZ,1D";
+     option.value= "sjfz,1d"; 
+     sel.add(option);
+   }
+
+   if(isModelInstalled("ctmhlabn") && isModelInstalled("ctmhsgbn") &&
+      isModelInstalled("ctmhvbn") && isModelInstalled("ctmhrbn") &&
+      isModelInstalled("ctmhibbn") && isModelInstalled("ctmhsmbn") &&
+      isModelInstalled("ctmhsbbn") && isModelInstalled("ctmhsbcbn") &&
+      isModelInstalled("ctmhstbn") && isModelInstalled("canvas")) {
+        option = document.createElement("option");
+        option.text = "CTM-H All Basins,CANVAS";
+        option.label = "CTM-H All Basins,CANVAS";
+        option.value= "ctmhlabn,ctmhsgbn,ctmhvbn,ctmhrbn,ctmhibbn,ctmhsmbn,ctmhsbbn,ctmhsbcbn,ctmhstbn,canvas";
+        sel.add(option);
+   }
+
+        option.value= "ctmhlabn,ctmhsgbn,ctmhvbn,ctmhrbn,ctmhibbn,ctmhsmbn,ctmhsbbn,ctmhsbcbn,ctmhstbn,ctmsi";
+        sel.add(option);
+   }
+***/
+
+   if(isModelInstalled("ctmhlabn") 
+	   && isModelInstalled("ctmhsmbn")
+	   && isModelInstalled("ctms5") ) {
+      option = document.createElement("option");
+      option.text = "CTM-H LA Basin,CTM-H Santa Maria Basin,CTM-S4.26";
+      option.label = "CTM-H LA Basin,CTM-H Santa Maria Basin,CTM-S4.26";
+      option.value= "ctmhlabn,ctmhsmbn,ctms5";
+      sel.add(option);
+   }
+
+  if(isModelInstalled("sfctm") && isModelInstalled("cca")) {
+   option = document.createElement("option");
+   option.text = "SFCTM,CCA,SF1D";
+   option.label = "SFCTM,CCA,SF1D";
+   option.value= "sfctm,cca,sf1d";
+   sel.add(option);
+   }
+
+   // All of them
+  if(all_model_list.length != 0) {
+   option = document.createElement("option");
+   option.text = "All models";
+   option.label = "All models";
+   option.value= all_model_list.toString();
+   sel.add(option);
+   }
+
+}
+
+// target_nm is abb_name
+function getModelIndex(nm) {
+   let target=nm.trim();
+   var tb=CTM_tb['models'];
+   var icnt=tb.length;
+   var i;
+   for(i=0; i<icnt; i++) {
+     var item=tb[i];
+     if(item['abb name'] == target) {
+        return i;
+     }
+  }
+  return -1;
+}
+
+// using id in CTM_tb
+function getModelDescriptionById(id) {
+   let tb=CTM_tb['models'];
+   let item=tb[id];
+   var descript=item['description'];
+   return descript;
+}
+
+function getModelDescriptionBriefById(id) {
+   let tb=CTM_tb['models'];
+   let item=tb[id];
+
+   let tmp=item['description_brief'];
+   if('description_brief' in item) { 
+       return item['description_brief'];
+   } else return null;
+}
+
+function getModelNameById(id) {
+   let tb=CTM_tb['models'];
+   let item=tb[id];
+   var name=item['name'];
+   return name;
+}
+function getModelAbbNameById(id) {
+   let tb=CTM_tb['models'];
+   let item=tb[id];
+   var name=item['abb name'];
+   return name;
+}
+
+// nm = 'abb name'
+// return unique index list into references array
+function getReferenceIndex(nm, olist,alist,rlist) {
+   let target=nm.trim();
+   let mlist=CTM_tb['references'];
+   let foo=mlist[0];
+   let mcnt=mlist.length;
+   for(let i=0; i<mcnt; i++) {
+      let fitem=mlist[i];
+      let nlist=fitem['name'];
+      let ncnt=nlist.length;
+      for(let j=0; j<ncnt; j++) {
+        if(nlist[j] == target) {
+          if(olist.includes(i)) {
+//            window.console.log("duplicate found..",i);
+            } else {
+             
+              if( 'author' in fitem ) {
+                olist.push(i);
+                alist.push(fitem['author']);
+                rlist.push(fitem['ref']);
+              }
+          }
+        }
+      }
+   }
+}
+
+function getReferenceByList(reflist,alist,rlist) {
+   let mlist=CTM_tb['references'];
+   let cnt=reflist.length;
+   for(let i=0; i<cnt; i++) {
+      let item=mlist[i];
+      if('author' in item) {
+        alist.push(item['author']);
+        rlist.push(item['ref']);
+      }
+   }
+   return alist,rlist;
+}
+
+function getInterpolatorIndex(nm) {
+   let target=nm.trim()
+   var tb=CTM_tb['interpolator'];
+   var icnt=tb.length;
+   var i;
+   for(i=0; i<icnt; i++) {
+     var item=tb[i];
+     if(item['abb name'] == target) {
+        return i;
+     }
+  }
+  return -1;
+}
+
+function getInterpolatorDescriptionById(id) {
+   let tb=CTM_tb['interpolator'];
+   let item=tb[id];
+   var descript=item['description'];
+   return descript;
+}
+
+function getInterpolatorNameById(id) {
+   let tb=CTM_tb['interpolator'];
+   let item=tb[id];
+   var name=item['name'];
+   return name;
+}
+
+function getInterpolatorAbbNameById(id) {
+   let tb=CTM_tb['interpolator'];
+   let item=tb[id];
+   var name=item['abb name'];
+   return name;
+}
+
+function getInterpolatorReferenceById(id) {
+   let tb=CTM_tb['interpolator'];
+   let item=tb[id];
+   if ('references' in item ) {
+     return item['references'];
+   } 
+   return undefined;
+}
+
+function getInterpolatorAuthorById(id) {
+   let tb=CTM_tb['interpolator'];
+   let item=tb[id];
+   if ('authors' in item ) {
+     return item['authors'];
+   } 
+   return undefined;
+}
+
+function getInterpolatorAuthorById(id) {
+   let tb=CTM_tb['interpolator'];
+   let item=tb[id];
+   if ('authors' in item ) {
+     return item['authors'];
+   } 
+   return undefined;
+}
+
+
+function get1DModelIndex(nm) {
+   let target=nm.trim();
+   var tb=CTM_tb['1D model'];
+   var icnt=tb.length;
+   var i;
+   for(i=0; i<icnt; i++) {
+     var item=tb[i];
+     if(item['abb name'] == target) {
+        return i;
+     }
+  }
+  return -1;
+}
+
+function get1DModelDescriptionById(id) {
+   let tb=CTM_tb['1D model'];
+   let item=tb[id];
+   var descript=item['description'];
+   return descript;
+}
+
+function get1DModelNameById(id) {
+   let tb=CTM_tb['1D model'];
+   let item=tb[id];
+   var name=item['name'];
+   return name;
+}
+
+function get1DModelAbbNameById(id) {
+   let tb=CTM_tb['1D model'];
+   let item=tb[id];
+   var name=item['abb name'];
+   return name;
+}
+
+function get1DModelReferenceById(id) {
+   let tb=CTM_tb['1D model'];
+   let item=tb[id];
+   if ('references' in item ) {
+     return item['references'];
+   } 
+   return undefined;
+}
+
+function get1DModelAuthorById(id) {
+   let tb=CTM_tb['1D model'];
+   let item=tb[id];
+   if ('authors' in item ) {
+     return item['authors'];
+   } 
+   return undefined;
+}
+
+function get1DModelAuthorById(id) {
+   let tb=CTM_tb['1D model'];
+   let item=tb[id];
+   if ('authors' in item ) {
+     return item['authors'];
+   } 
+   return undefined;
+}
+
+
+
+function getModelColor(nm) {
+// this is an optional field, vs30/topo etree map
+   let target=nm.trim();
+   var tb=CTM_tb['models'];
+   var icnt=tb.length;
+   var i;
+   for(i=0; i<icnt; i++) {
+     var item=tb[i];
+     if(item['abb name'] == target) {
+        var color=item['color'];
+        return color;
+     }
+  }
+  return "black";
+}
+
+// this is an optional field, vs30/topo etree map
+function getModelMap(nm) {
+   let target=nm.trim();
+   var tb=CTM_tb['models'];
+   var icnt=tb.length;
+   var i;
+   for(i=0; i<icnt; i++) {
+     var item=tb[i];
+     if(item['abb name'] == target) {
+        if(item.has('map')) {
+           return item['map'];
+        }
+     }
+  }
+  return NULL;
+}
+
+function makeLatlngsCoordinate(nm) {
+   let target=nm.trim();
+   var ret=[];
+   var tb=CTM_tb['models'];
+   var icnt=tb.length;
+   var lon, lat;
+   var i,j;
+   for(i=0; i<icnt; i++) {
+     var item=tb[i];
+     if(item['abb name'] == target) {
+        var coord=item['coordinates'];
+        var jcnt=coord.length;
+        for(j=0;j<jcnt;j++) {
+          var c=coord[j];
+          lon=c['lon'];
+          lat=c['lat'];
+          ret[ret.length]=([lat, lon]);      
+        }
+//        window.console.log(ret);
+        return ret;
+      }
+   }
+   return ret;
+}
+
+function makeModelTable() {
+   var tb=CTM_tb['models'];
+   var cnt=tb.length;
+   var i;
+   var tbhtml="<table><tbody><tr><td style=\"border:1px solid white;\">CTM Model Table</td></tr></tbody></table>";
+   tbhtml=tbhtml+"<div class=\"ctm-table\"><table><tbody>";
+   tbhtml=tbhtml+"<tr><th style=\"width:8vw\"><b>Model</b></th><th style=\"width:6vw\"><b>CTM abbreviation</b></th><th style=\"width:40vw\"><b>Description</b></th></tr>";
+
+   for( i=0; i<cnt; i++) {
+     var item=tb[i];
+     var mname=item['name'];
+     var aname=item['abb name'];
+     var pname=item['path name'];
+     if(isModelInstalled(pname)) {
+       var descript=item['description'];
+       var t="<tr><td style=\"width:6vw\">"+mname+"</td><td style=\"width:6vw\">"+aname+"</td><td style=\"width:40vw\">"+descript+"</td></tr>";
+       tbhtml=tbhtml+t;
+     }
+   }
+   tbhtml=tbhtml+"</tbody></table></div>";
+   return tbhtml;
+}
+
+function _getModelItemWithID(id) {
+   var tb=CTM_tb['models'];
+   var cnt=tb.length;
+   var i;
+   for(i=0; i<cnt;i++) {
+      var model=tb[i];
+      if(model['id'] == id) {
+        return model;
+      }
+   }
+   return undefined;
+}
+
+function getModelNameWithID(id) {
+   var item=_getModelItemWithID(id);
+   if(item != undefined) {
+       var n= item['name'];
+       return n;
+   }
+   return undefined;
+}
+
+function getModelNameWithType(t) {
+   // t could be multiple, "albacore,ctms"
+   var rt="";
+   var mlist=t.split(',');
+   var mcnt=mlist.length;
+   var tlist=CTM_tb['models'];
+   var tcnt=tlist.length;
+   var i,j;
+
+   for(i=0;i<mcnt;i++) {
+      for(j=0; j<tcnt;j++) {
+         var target=tlist[j];
+         if(target['abb name'] == mlist[i]) {
+            rt=rt+target['name'];
+            if(mcnt>1 && i!=(mcnt-1))
+               rt=rt+", ";
+            break;
+	 } 
+      }
+      if(j == tcnt) { // not found
+         rt=rt+mlist[i];
+      }
+   }
+   if(rt=="") {
+       rt=undefined;
+   }
+
+   return rt;
+}
+function getZModeNameWithType(t) {
+   var tb=CTM_tb['zmodes'];
+   var cnt=tb.length;
+   var i;
+   for(i=0; i<cnt;i++) {
+      var zmode=tb[i];
+      if(zmode['value'] == t) {
+        return zmode['mode name'];
+      }
+   }
+   return undefined;
+}
+
+function getModelColorWithID(id) {
+   var item=_getModelItemWithID(id);
+   if(item != undefined) {
+       var c= item['color'];
+       return c;
+   }
+   return undefined;
+}
+
+function getModelCoordinatesWithID(id) {
+   var item=_getModelItemWithID(id);
+   if(item != undefined) {
+       var coord= item['coordinates'];
+       return coord;
+   }
+   return undefined;
+}
+
+function getAllModelNames() {
+   var ret=[];
+   var tb=CTM_tb['models'];
+   var cnt=tb.length;
+   var i,item,aname;
+   for(i=0; i<cnt; i++) {
+     item=tb[i]; 
+     aname=item['abb name'];
+     ret.push(aname);
+   }
+   return ret;
+}
+
+function showInTable(key) {
+   var tb=CTM_tb['descript'];
+   var cnt=tb.length;
+   for( i=0; i<cnt; i++) {
+     var item=tb[i];
+     var label=item['id'];
+     if(label == key) {
+       return item['show'];
+     }
+  }
+  window.console.log("ERROR, showInTable, no such key",key);
+  return 0;
+}
+
+function makeParametersTable() {
+   var tb=CTM_tb['descript'];
+   var cnt=tb.length;
+   var i;
+   var tbhtml="<table><tbody><tr><td style=\"border:1px solid white;\">CTM Parameters Table</td></tr></tbody></table>";
+   tbhtml=tbhtml+"<div class=\"ctm-table\"><table><tbody>";
+   tbhtml=tbhtml+"<tr><th style=\"width:10vw\"><b>Parameter</b></th><th style=\"width:45vw\"><b>Description</b></th></tr>";
+   for( i=0; i<cnt; i++) {
+     var item=tb[i];
+     var label=item['id'];
+     var descript=item['descript'];
+     if( item['show'] ) {
+       var t="<tr><td style=\"width:10vw\">"+label+"</td><td style=\"width:45vw\">"+descript+"</td></tr>";
+       tbhtml=tbhtml+t;
+     }
+   }
+   tbhtml=tbhtml+"</tbody></table></div>";
+   return tbhtml;
+}
+
