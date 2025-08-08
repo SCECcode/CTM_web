bbacd78b8b15306257b874bd4f21b6d9ce2d3174
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2025-08-08 14:24:46 +0000

    wip

--- schema/scripts/convert-shp.sh
+++ schema/scripts/convert-shp.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env sh
 
+## ./convert-shp.sh
+##        mv *.sql to  schema/sql/target
+##
 ## convert from shp file content to sql import script 
 ## shp2pgsql is part of postgres sql toolkit
+ 
+##   AWSPATH="https://s3-us-west-2.amazonaws.com/files.scec.org/s3fs-public/projects/ctm/CTM2/Lee_2025"
+
+PWD=`pwd`
 
 echo "running "$0
-DATATYPE=$1
-. ./common.sh
 
-shp2pgsql -I -s GCS_WGS_1984 ${CFMLOC}.shp nonblind_trace_tb > ${CFMTYPE}_traces_nonblind.sql
-shp2pgsql -I -s GCS_WGS_1984 ${CFMLOC_BLIND}.shp blind_trace_tb > ${CFMTYPE}_traces_blind.sql
-
-#cp $CFMPATH/doc/*.xlsx .
-
+echo "  --setup for lee2025"
+shp2pgsql -I -s GCS_WGS_1984 ${PWD}"/../data/Lee_2025/shp/Lee_2025_extent.shp" lee2025_tb > lee2025_tb.sql
+mv lee2025_tb.sql ../sql
+echo "  --setup for shinevar2018"
+shp2pgsql -I -s GCS_WGS_1984 ${PWD}"/../data/Shinevar_2018/shp/Shinevar_2018_extent.shp" shinevar2018_tb > shinevar2018_tb.sql
+mv shinevar2018_tb.sql ../sql
 
 #>>MeiPro:CFM5_preferred_traces mei$ ./convert-shp.sh
 #>>Shapefile type: ArcZ
